dc4fabf12b068b3c87399edeaa4c420eb6c21ba8
levywalk <levy.walk.co@gmail.com>
2012-12-09 13:23:18 +1100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyVeryFirstApp::Application.config.secret_token = '4adcbd7deba6a0427ef453f1963f3c1b883e77d3309fbd735cd6d8408a0645e9a350436d67f78fbc6aea2a2e9be22f0165868ed566c57348599d5d2defe45216'
